a15d573af719d857d8b1868dbd0b846322c38067
tell1to <155204173+tell1to@users.noreply.github.com>
2024-11-16 17:14:38 -0500

    Add files via upload

---
+++ dist/config/config.key.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Configuration = void 0;
+var Configuration;
+(function (Configuration) {
+    Configuration["PORT"] = "PORT";
+    Configuration["HOST"] = "HOST";
+    Configuration["USERNAME"] = "USERNAME";
+    Configuration["PASSWORD"] = "PASSWORD";
+    Configuration["DATABASE"] = "DATABASE";
+})(Configuration || (exports.Configuration = Configuration = {}));
+//# sourceMappingURL=config.key.js.map
