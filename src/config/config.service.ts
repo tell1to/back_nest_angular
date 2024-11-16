efbef02838b4a5a73ca773543aea1262c6ff1221
tell1to <155204173+tell1to@users.noreply.github.com>
2024-11-16 16:12:56 -0500

    database provider

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -4,33 +4,27 @@ import { parse } from 'dotenv';
 
 @Injectable()
 export class ConfigService {
-    private readonly envConfig:{[key:string]:string}
-    constructor(){
-        const isDevelomentEnv = process.env.NODE_ENV !== 'production'
-        if(isDevelomentEnv){
-            const envFilePath= __dirname +'/../../.env.development'
+    private readonly envConfig: { [key: string]: string }
+    constructor() {
+        const isDevelopmentEnv = process.env.NODE_ENV !== 'production'
+        if (isDevelopmentEnv) {
+            const envFilePath = __dirname + '/../../.env.development'
             const existsPath = fs.existsSync(envFilePath)
-            if(!existsPath){
+            if (!existsPath) {
                 console.log('.env.development no existe DEVELOPMENT')
-                process.exit(0)
             }
-            this.envConfig=parse(fs.readFileSync(envFilePath))
-        }
-        else
-        {
-            const envFilePath= __dirname +'/../../.env.production'
+            this.envConfig = parse(fs.readFileSync(envFilePath))
+        } else {
+            const envFilePath = __dirname + '/../../.env.production'
             const existsPath = fs.existsSync(envFilePath)
-            if(!existsPath){
+            if (!existsPath) {
                 console.log('.env.production no existe PRODUCTION')
-                process.exit(0)
             }
-            this.envConfig=parse(fs.readFileSync(envFilePath))
+            this.envConfig = parse(fs.readFileSync(envFilePath))
         }
-        
-        
     }
-    get(key:string):string{
+    get(key: string): string {
         return this.envConfig[key];
     }
-
 }
+
